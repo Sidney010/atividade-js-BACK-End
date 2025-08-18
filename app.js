ce91b49c475edb88d183b60a09f1d6481409e577
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2025-08-18 16:09:30 -0300

    Finalizado

---
+++ app.js
@@ -0,0 +1,58 @@
+
+// for(let i = 10; i <= 200; i++){
+//     console.log(`Números: ${i}`)
+// }
+
+// let i = 200
+// do{
+//    console.log(`Número: ${i}`) 
+//     i--
+// } while (
+//     i >= 10
+// )
+
+
+
+
+// let primeiroNumero = prompt('Digite o primeiro número')
+// let segundoNumero = prompt('Digite o segundo número')
+//
+// if(primeiroNumero <= segundoNumero){
+//     while (primeiroNumero <= segundoNumero){
+//         console.log(primeiroNumero)
+//         primeiroNumero++
+//     }
+// }else{
+//     while(segundoNumero <= primeiroNumero){
+//         console.log(segundoNumero)
+//         segundoNumero++
+//     }
+// }
+
+
+
+
+// let primeiroNumero = prompt('Digite o primeiro número')
+// let segundoNumero = prompt('Digite o segundo número')
+// if(primeiroNumero >= segundoNumero){
+//     while (primeiroNumero >= segundoNumero){
+//         console.log(primeiroNumero)
+//         primeiroNumero--
+//     }
+// }else{
+//     while(segundoNumero >= primeiroNumero){
+//         console.log(segundoNumero)
+//         segundoNumero--
+//     }
+// }
+
+
+// let numeroTabuada = prompt('Digite o número da tabuada:')
+// for(let i = 0; i < 11; i++){
+//     let resultado = numeroTabuada * i 
+//     console.log(`${numeroTabuada} x ${i} = ${resultado}`)
+// }
+
+
+
+
